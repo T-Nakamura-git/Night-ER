97aea797cb94f8200157ca4e1d87ca9e4c6b4c80
T-Nakamura-git <unbreakablechain@gmail.com>
2018-12-18 06:36:26 +0000

    heroku deploy

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+  def index
+  end
+  
+  def toppage
+  end
+  
+  def detail
+  end
+  
+  def hpdb
+  end  
+  
+  def theory
+  end  
+  
+  def info
+  end  
+  
+  def blog
+  end  
+  
+  def form
+  end  
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
+  root 'home#index'
   resources :columns
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :top
-  root 'top#index'
+  get 'toppage', to: 'home#toppage'
   get 'search', to: 'top#search'
+  get 'detail', to: 'home#detail'
+  get 'hpdb', to: 'home#hpdb'
+  get 'theory', to: 'home#theory'
+  get 'blog', to: 'home#blog'    
+  get 'info', to: 'home#info'
+  get 'form', to: 'home#form'
 end
+
+
+
