c1578f7cfb6a57798cdf0a4defcca76731845d76
T-Nakamura-git <unbreakablechain@gmail.com>
2018-12-20 04:05:54 +0000

    database.yml

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   root 'home#index'
   resources :columns
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'toppage', to: 'home#toppage'
-  get 'search', to: 'top#search'
-  get 'detail', to: 'home#detail'
-  get 'hpdb', to: 'home#hpdb'
-  get 'theory', to: 'home#theory'
-  get 'blog', to: 'home#blog'    
-  get 'info', to: 'home#info'
-  get 'form', to: 'home#form'
+  get '/toppage', to: 'home#toppage'
+  get '/search', to: 'top#search'
+  get '/detail', to: 'home#detail'
+  get '/hpdb', to: 'home#hpdb'
+  get '/theory', to: 'home#theory'
+  get '/blog', to: 'home#blog'    
+  get '/info', to: 'home#info'
+  get '/form', to: 'home#form'
 end
 
 
